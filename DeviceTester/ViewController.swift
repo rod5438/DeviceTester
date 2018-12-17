a3de71505bd2077116684a046a7b5cbabad8d760
Jason Wu <rod5438@gmail.com>
2018-12-17 13:41:47 +0800

    [What] Add project [Why] [How] [Risk]

---
+++ DeviceTester/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeviceTester
+//
+//  Created by Jason Wu on 2018/12/17.
+//  Copyright © 2018 Jason Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
